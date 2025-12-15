3d307e21ac42a6be2daed3895bd04fc90ab9f3dc
Simonjs <34932488+truesimonjs@users.noreply.github.com>
2025-12-15 13:44:54 +0100

    readded singleton thing to program.cs

--- Zealand_semester1_eksamen/Program.cs
+++ Zealand_semester1_eksamen/Program.cs
@@ -1,10 +1,12 @@
 
 
+using Zealand_semester1_eksamen.SJS;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Razor Pages support
 builder.Services.AddRazorPages();
-
+builder.Services.AddSingleton<IBoatServiceSJS, BoatServiceSJS>();
 var app = builder.Build();
 
 // Enable static files (css, js)
